777ee193979fff880fe0cee00c88a76d63e9aa8e
sriganesh8217-cmyk <sriganesh8217@gmail.com>
2026-03-02 14:08:11 +0530

    change

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 from google import genai
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 import os
+
 
 load_dotenv()
 
 app = FastAPI()
 
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (adjust for production)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 def home():
